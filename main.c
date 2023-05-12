247003feb9a7f0c52527cd7f54a9c5040b0b1190
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-11 18:24:13 -0600

    Output prefix for selling

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
+#include <stdio.h>
+
+enum {
+	OPEN,
+	CLOSE
+};
+
+const char pair[2] = {
+	'[',
+	']'
+};
+
+const char c = 'S';
+
+void err(char* msg) {
+	printf("Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc != 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("%c%c%c", pair[OPEN], c, pair[CLOSE]);
+
 	return 0;
 }
